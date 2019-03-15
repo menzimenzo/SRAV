f41f4571589a65220cd09bcdc245953b0fbbbc7d
SCHERER <laurent.scherer@sante.gouv.fr>
2019-03-15 16:22:22 +0100

    Mise à jour des logos + nettoyage images(bis)

--- front-end/nuxt.config.js
+++ front-end/nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Savoir Rouler',
+    title: 'Savoir rouler à vélo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
