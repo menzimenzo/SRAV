735d06cf07239fb2a7802728ca478a50a97a815d
SCHERER <laurent.scherer@sante.gouv.fr>
2019-03-19 17:33:50 +0100

    Complément inscription

--- back-end/routes/structures.js
+++ back-end/routes/structures.js
@@ -2,21 +2,50 @@ const express = require('express');
 const router = express.Router();
 const pgPool = require('../pgpool').getPool();
 
+
+
+router.get('/structure', 
+    async function (req, res) {
+        const id = req.query.id;
+        console.log('Recherche d\'une structure - id = ' + id);
+    
+        const requete =`SELECT * from Structure where str_id=${id}`;
+        console.log(requete)
+    
+        pgPool.query(requete, (err, result) => {
+            if (err) {
+                console.log(err.stack);
+                return res.status(400).json('erreur lors de la récupération de la structure');
+            }
+            else {
+                const structures = result.rows[0];
+
+                //const intervention = result.rows && result.rows.length && result.rows[0];
+                if (!structures) {
+                    return res.status(400).json({ message: 'Structure inexistante' });
+                }
+                return res.status(200).json({ structures });
+            }
+        })
+    });
+    
 router.get('/',
 
     function (req, res) {
-        console.log('Getting result');
-        
+
+        console.log('1-Getting result');
+        // La méthode get est appelée sans paramètre : On retourne la liste
         pgPool.query(
             'SELECT * FROM structure',
             function (err, result) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log('Fin de la transaction ', result.length);
+                    console.log('Fin de la transaction (sans paramètres)', result.length);
                     return res.send(result.rows);
                 }
             });
+
         
     });
 
