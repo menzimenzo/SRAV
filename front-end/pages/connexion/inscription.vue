e7cd34681ae2c325539ef82b345f94bc5a42eb88
rverdier <rverdier@synaltic.fr>
2019-03-25 15:26:49 +0100

    Validation de la confirmation d'inscription avec vee-validate

--- front-end/pages/connexion/inscription.vue
+++ front-end/pages/connexion/inscription.vue
@@ -27,47 +27,83 @@
               </b-form-group>
             </b-card>
             <b-card class="mb-3">
-              <b-form-group
-                id="emailInputGroup"
-                label="Email:"
-                label-for="emailInput" required>
+              <b-form>
+                <b-form-group
+                  id="emailInputGroup"
+                  label="Email:"
+                  label-for="emailInput" required>
 
-                <b-form-input
-                  id="emailInput"
-                  type="email" v-model="user.mail"
-                  required
-                  placeholder="Email" />
-              </b-form-group>
+                  <b-form-input
+                    id="emailInput"
+                    type="email" v-model="user.mail"
+                    required
+                    name="mail"
+                    v-validate="{required: true, email: true}"
+                    aria-describedby="emailFeedback"
+                    placeholder="Email" 
+                    :state="validateState('mail')"
+                    />
+
+                  <b-form-invalid-feedback id="emailFeedback">
+                    L'email est obligatoire et doit être valide.
+                  </b-form-invalid-feedback>
+
+                </b-form-group>
 
 
-              <b-form-group required id="structNationaleGroup" label="Structure nationale:" label-for="structNatSelect">
-                <b-form-select id="structNatSelect"  v-model="user.structureId">
-                  <option v-for="structure in structures" :key="structure.str_id" :value="structure.str_id">{{ structure.str_libelle}}</option>
-                </b-form-select>
-              </b-form-group>
+                <b-form-group required id="structNationaleGroup" label="Structure nationale:" label-for="structNatSelect">
+                  <b-form-select id="structNatSelect"  v-model="user.structureId"
+                    v-validate="{required: true}"
+                    name="struct"
+                    :state="validateState('struct')"
+                    aria-describedby="structFeedback">
+                    <option :value="null"> Veuillez choisir votre structure...</option>
+                    <option v-for="structure in structures" :key="structure.str_id" :value="structure.str_id">{{ structure.str_libelle}}</option>
+                  </b-form-select>
+                  <b-form-invalid-feedback id="structFeedback">
+                    Il est nécessaire de choisir une structure.
+                  </b-form-invalid-feedback>
+                </b-form-group>
 
-              <b-form-group id="structLocaleGroup" label="Structure Locale:" required v-if="isFederation(user.structureId)" label-for="structLocaleInput">
-                <b-form-input
-                  id="structLocaleInput"
-                  type="text" v-model="user.structureLocale"
-                  required
-                  placeholder="Nom de la structure" />
-              </b-form-group>
+                <b-form-group id="structLocaleGroup" label="Structure Locale:" required v-if="isFederation(user.structureId)" label-for="structLocaleInput">
+                  <b-form-input
+                    v-validate="{required: isFederation(user.structureId)}"
+                    name="structLoc"
+                    :state="validateState('structLoc')"
+                    aria-describedby="structLocFeedback"
+                    id="structLocaleInput"
+                    type="text" v-model="user.structureLocale"
+                    required
+                    placeholder="Nom de la structure" />
+                  <b-form-invalid-feedback id="structLocFeedback">
+                    La structure local est obligatoire.
+                  </b-form-invalid-feedback>
+                </b-form-group>
 
-              <b-form-group id="legalCheckGroup">
-                <b-form-checkbox-group v-model="isLegalChecked" id="legalCheck">
-                  <b-form-checkbox value="true">« Intervenant du <b>Savoir Rouler à Vélo</b>, je m’engage à construire et réaliser mes sessions d’apprentissage sur la base du socle commun <b>Savoir Rouler à Vélo</b> et à vérifier l’acquisition de l’ensemble des compétences attendues du bloc 1, 2 et 3 pour délivrer l’attestation <b>Savoir Rouler à Vélo</b>». 
-                  </b-form-checkbox>
-                </b-form-checkbox-group>
-              </b-form-group>
+                <b-form-group id="legalCheckGroup">
+                  <b-form-checkbox-group v-model="isLegalChecked" id="legalCheck" :state="validateState('legalCheck')"
+                      aria-describedby="legalFeedback"
+                      v-validate="{is: 'true'}" name="legalCheck"  >
+                    <b-form-checkbox value="true" 
+                    >
+                      « Intervenant du <b>Savoir Rouler à Vélo</b>, je m’engage à construire et réaliser mes sessions d’apprentissage sur la base du socle commun <b>Savoir Rouler à Vélo</b> et à vérifier l’acquisition de l’ensemble des compétences attendues du bloc 1, 2 et 3 pour délivrer l’attestation <b>Savoir Rouler à Vélo</b>».<span style="color: red;">*</span> 
+                    </b-form-checkbox>
+                  </b-form-checkbox-group>
+                  <b-form-invalid-feedback id="legalFeedback">
+                    Il est obligatoire de valider les conditions légales.
+                  </b-form-invalid-feedback>
+                </b-form-group>
+
+                <b-form-group>
+                  <span style="color: red;">*</span> : Champ obligatoire
+                </b-form-group>
+                <div class="mb-3 text-right">
+                  <b-button  @click="confirmRegistration" variant="success" :disabled="errors.any()" >
+                    Je valide mon compte
+                  </b-button>
+                </div>
+              </b-form>
             </b-card>
-            <div class="mb-3 text-right">
-              <b-button variant="success" @click="confirmRegistration()" :disabled="isFormDisabled" >
-                Je valide mon compte
-              </b-button>
-            </div>
-            
-
         </b-col>
       </b-row>
     </b-container>
@@ -87,14 +123,17 @@ export default {
   methods: {
     // Validation de l'inscription
     confirmRegistration(){
-      const url = process.env.API_URL + '/connexion/verify'
-
-      return this.$axios.$post(url, this.user)
-      .then(async response => {
-        await this.$store.dispatch('set_utilisateur', response.user);
-        this.$router.push('/interventions')
-      }).catch(err => {
-        console.log(err)
+      this.$validator.validateAll().then(isValid => {
+        if(isValid){
+          const url = process.env.API_URL + '/connexion/verify'
+          return this.$axios.$post(url, this.user)
+          .then(async response => {
+            await this.$store.dispatch('set_utilisateur', response.user);
+            this.$router.push('/interventions')
+          }).catch(err => {
+            console.log(err)
+          })
+        }
       })
     },
     // true si la structure sélectionnée est une fédération
@@ -106,22 +145,20 @@ export default {
        if(!structure){return false}
        return structure.str_federation
       
+    },
+    validateState(ref) {
+      if(!this.veeFields){ return null}
+      if (this.veeFields[ref] && (this.veeFields[ref].dirty || this.veeFields[ref].validated)) {
+        return !this.errors.has(ref)
+      }
+      return null
     }
   },
   async mounted(){
     await this.$store.dispatch('get_structures')
   },
   computed:{
-    ...mapState(['structures']),
-    // Valide les champs requis
-    isFormDisabled(){
-      var isDisabled = false
-      isDisabled = isDisabled || this.isLegalChecked == 'false' || !this.isLegalChecked
-      isDisabled = isDisabled || this.user.structureLocale == '' || !this.user.structureLocale
-      isDisabled = isDisabled || this.user.mail == '' || !this.user.mail
-      return isDisabled
-      
-    }
+    ...mapState(['structures'])
   }
 
 };
--- front-end/plugins/vee-validate.js
+++ front-end/plugins/vee-validate.js
@@ -4,10 +4,12 @@ import VeeValidate, { Validator } from 'vee-validate'
 
 import fr from 'vee-validate/dist/locale/fr'
 
-
-
 // Install the Plugin and set the locale.
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, {
+    inject: true,
+    // Important to name this something other than 'fields'
+    fieldsBagName: 'veeFields'
+})
 
 // Add locale helper.
 Validator.localize('fr', fr)
