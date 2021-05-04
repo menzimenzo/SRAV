78b43b66560ef4159dbbe6ec8476dcd7ff5c2fc5
lscherer <laurent_scherer@hotmail.com>
2021-05-04 11:20:56 +0200

    Création des packages

---
+++ packages/prepare-package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Shell de préparation du package de déploiement pour livraison
+
+cp -r ../front-end ./
+rm -r ./front-end/node_modules
+rm ./front-end/pm2.integ.json
+
+cp -r ../back-end ./
+rm -r ./back-end/node_modules
+rm ./back-end/pm2.integ.json
+
+cp -r ../mail-server ./
+rm -r ./mail-server/node_modules
+rm ./mail-server/pm2.integ.json
+
+tar -zcvf srav_package.tar.gz  front-end back-end mail-server
+
+rm -r ./front-end
+rm -r ./back-end
+rm -r ./mail-server
+
