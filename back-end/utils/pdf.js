a8889dcd672618898a405c0b5fc85a0c418a78f5
lscherer <laurent_scherer@hotmail.com>
2022-05-04 17:25:02 +0200

    Ajout de la structure co-réalisatrice sur l'attestation

--- back-end/utils/pdf.js
+++ back-end/utils/pdf.js
@@ -7,74 +7,98 @@ const { lineGap } = require('pdfkit');
 const log = logger(module.filename)
 const pgPool = require('../pgpool').getPool();
 
-function generate(id,nbenfants,dateintervention, ustid) {
+function generate(id,nbenfants,dateintervention, ustid,strcorealisatrice) {
    var nbzero;
    var idformate = "";
     // Récupération du logo s'il y en a un 
     pgPool.query(
-    `SELECT encode(stl_document, 'base64') as image, str_logo_proportion as proportion, str_logo_pos_vertical as y_pos, str_logo_pos_horizontal as x_pos 
-            FROM structure_logo stl
-            INNER JOIN structure str on str.str_id = stl.str_id and str.str_partenaire_titre = true
-            INNER JOIN uti_str ust on ust.str_id = str.str_id and ust.ust_id = $1`, [ustid],
-    function (err, results) {
+        `SELECT encode(stl_document, 'base64') as image, str_logo_proportion as proportion, str_logo_pos_vertical as y_pos, str_logo_pos_horizontal as x_pos 
+                FROM structure_logo stl
+                INNER JOIN structure str on str.str_id = stl.str_id and str.str_partenaire_titre = true
+                INNER JOIN uti_str ust on ust.str_id = str.str_id and ust.ust_id = $1`, [ustid],
+    function (err, results) 
+    {
         if (err) {
             log.w('::get - erreur',err);
-        } else {
-            log.i('::get - Done, fin de la transaction documents');
+        } 
+        else 
+        {
+            pgPool.query(
+                `SELECT str_libelle  
+                        FROM structure 
+                        WHERE str_id = $1`, [strcorealisatrice],
+            function (err, resultsstr) 
+            {
+                if (err) {
+                    log.w('::get - erreur',err);
+                } 
+                else {
+                    idformate = id.toString();
+                    for (nbzero=0;nbzero<7-id.toString().length;nbzero++){
+                        idformate = "0" + idformate;
+                    }
 
-            idformate = id.toString();
-            for (nbzero=0;nbzero<7-id.toString().length;nbzero++){
-                idformate = "0" + idformate;
-            }
-            var doc = new PDFDocument({
-                size: 'A4',
-                'dpi':400,
-                layout: 'landscape' // default is portrait
-            });
-        
-            // Pipe its output somewhere, like to a file or HTTP response
-            // See below for browser usage
-            doc.pipe(fs.createWriteStream(`${config.pathAttestation}${idformate}.pdf`));    
-        
-            var indexpage;
-            for (indexpage = 1; indexpage <= nbenfants; indexpage++) {
-                doc.image("./assets/FondSavoirRouler.png", 0,0, {
-                    cover: [841.89 , 595.28],
-                    layout : 'landscape'
-                });
-        
-                var numattestation = "";
-                var nbzero;
-                var dateaffichee = ""
-            
-                // Formatage du numéro correspondant à chaque page
-                var indexpageformate = indexpage.toString();
-                for (nbzero=0;nbzero<4-indexpage.toString().length;nbzero++){
-                    indexpageformate = "0" + indexpageformate;
-                }
-                // Formatage final du numéro d'intervention
-                numattestation = idformate + indexpageformate;
-                // Formatage de la date
-                dateaffichee = dateintervention.substr(8,2)+"/"+dateintervention.substr(5,2)+"/"+dateintervention.substr(0,4);
-                doc.fontSize(14);
-                doc.text(numattestation,577,352.5,{align:'center'});
-                doc.text(dateaffichee,409,422,{align:'center'});
+                    var doc = new PDFDocument({
+                        size: 'A4',
+                        'dpi':400,
+                        
+                        layout: 'landscape', // default is portrait
+                        margins: {
+                            bottom: 0,
+                        }
+                    });
+                                // Pipe its output somewhere, like to a file or HTTP response
+                    // See below for browser usage
+                    doc.pipe(fs.createWriteStream(`${config.pathAttestation}${idformate}.pdf`));    
+                
+                    var indexpage;
+                    for (indexpage = 1; indexpage <= nbenfants; indexpage++) {
+                        doc.image("./assets/FondSavoirRouler.png", 0,0, {
+                            cover: [841.89 , 595.28],
+                            layout : 'landscape'
+                        });
+                
+                        var numattestation = "";
+                        var nbzero;
+                        var dateaffichee = ""
+                    
+                        // Formatage du numéro correspondant à chaque page
+                        var indexpageformate = indexpage.toString();
+                        for (nbzero=0;nbzero<4-indexpage.toString().length;nbzero++){
+                            indexpageformate = "0" + indexpageformate;
+                        }
+                        // Formatage final du numéro d'intervention
+                        numattestation = idformate + indexpageformate;
+                        // Formatage de la date
+                        dateaffichee = dateintervention.substr(8,2)+"/"+dateintervention.substr(5,2)+"/"+dateintervention.substr(0,4);
+                        doc.fontSize(14);
+                        doc.text(numattestation,630,352.5,{align:'left'});
+                        doc.text(dateaffichee,555,422.5,{align:'left'});
 
-                // Logo du partenaire titre s'il est coché
-                if (results.rows.length != 0) {
-                    var fileContents = results.rows[0].image
-                    doc.image(new Buffer.from(fileContents, 'base64'), results.rows[0].x_pos,results.rows[0].y_pos, {
-                        scale: results.rows[0].proportion/100
-                    });        
+                        // Logo du partenaire titre s'il est coché
+                        if (results.rows.length != 0) {
+                            var fileContents = results.rows[0].image
+                            doc.image(new Buffer.from(fileContents, 'base64'), results.rows[0].x_pos,results.rows[0].y_pos, {
+                                scale: results.rows[0].proportion/100
+                            });        
+                        }
+                        // Co-Réalisation 
+                        if (resultsstr.rows.length != 0) {
+                            doc.text("Co-réalisation avec le partenaire «"+resultsstr.rows[0].str_libelle+"»",350,517,{align:'right', width:430});
+                        }
+
+                        // dernière page ? pour ne pas ajouter de page vide
+                        if (indexpage <= nbenfants-1) {
+                            doc.addPage();
+                        }
+                    }
+                    // Finalize PDF file
+                    log.i('::get - Done, fin de la transaction documents');
+
+                    doc.end();
+                    return id;       
                 }
-                // dernière page ? pour ne pas ajouter de page vide
-                if (indexpage <= nbenfants-1) {
-                    doc.addPage();
-                }
-             }
-             // Finalize PDF file
-             doc.end();
-             return id;            
+            });       
         }
     });            
 
