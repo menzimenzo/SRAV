e9d4f466c8f41a6c8369217891512a04e45c4d4a
lscherer <laurent_scherer@hotmail.com>
2023-02-01 11:43:04 +0100

    version 1.1.10

--- front-end/components/footer.vue
+++ front-end/components/footer.vue
@@ -3,7 +3,7 @@
     <b-container>
       <b-row>
           <b-col cols="12 text-center">
-              Savoir Rouler A Vélo - Version 1.1.8
+              Savoir Rouler A Vélo - Version 1.1.10
           </b-col>
           <b-col cols="12 text-center">
               Contact :<i class="material-icons ml-2 mr-1" >mail</i>SRAV@sports.gouv.fr
